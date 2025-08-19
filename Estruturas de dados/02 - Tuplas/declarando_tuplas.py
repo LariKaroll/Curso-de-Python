2bda65c1a40a6324e70a351dc796bca6136ad522
LariKaroll <larikarol108@gmail.com>
2025-08-19 15:47:51 -0300

    doc: Aprendendo sobre tupla

--- Estruturas de dados/02 - Tuplas/declarando_tuplas.py
+++ Estruturas de dados/02 - Tuplas/declarando_tuplas.py
@@ -5,4 +5,11 @@ tupla = (1,2,3,4,5) # Declarando uma tupla com 5 elementos
 
 print(tupla)  # Imprimindo a tupla
 
-tupla[0] = 10  # Tentativa de alterar o primeiro elemento da tupla (isso causará um erro)    
+tupla.count(1)  # Contando quantas vezes o número 1 aparece na tupla
+tupla.index(3)  # Encontrando o índice do número 3 na tupla
+len(tupla)  # Obtendo o tamanho da tupla
+
+#tupla[0] = 10  # Tentativa de alterar o primeiro elemento da tupla (isso causará um erro)  
+
+carros = ("gol") 
+print(isinstance(carros, tuple))  
